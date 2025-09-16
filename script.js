8e3b86ea0aeea50bcc4707c3d2d02cf35f443328
Elisaftrn2911 <bobac370@gmail.com>
2025-09-16 13:48:44 +0000

    Feature: add simple Add to Cart button

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const cart = [];
 const products = [
   { id: 1, name: 'Kaos', price: 100000 },
   { id: 2, name: 'Topi', price: 50000 },
@@ -6,6 +7,14 @@ const products = [
 const ul = document.getElementById('products');
 products.forEach(p => {
   const li = document.createElement('li');
-  li.textContent = `${p.name} - Rp ${p.price}`;
+  li.textContent = `${p.name} - Rp ${p.price} `;
+  const btn = document.createElement('button');
+  btn.textContent = 'Add to Cart';
+  btn.onclick = () => {
+    cart.push(p);
+    alert(p.name + ' ditambahkan ke keranjang');
+    console.log(cart);
+  };
+  li.appendChild(btn);
   ul.appendChild(li);
 });
